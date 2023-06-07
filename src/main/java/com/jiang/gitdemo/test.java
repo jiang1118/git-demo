8c16dd1e1b1626522ff2ded53f4cd35151083aad
JiangShuoKun <923481817@qq.com>
2023-06-07 18:03:26 +0800

    idea添加暂存区3

--- src/main/java/com/jiang/gitdemo/test.java
+++ src/main/java/com/jiang/gitdemo/test.java
@@ -10,5 +10,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("测试");
         System.out.println("测试2");
+        System.out.println("测试3");
     }
 }
