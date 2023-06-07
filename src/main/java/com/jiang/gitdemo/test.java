68f38cba592e3aa28f1884849f6c0c7e2268f2a8
JiangShuoKun <923481817@qq.com>
2023-06-07 18:01:57 +0800

    idea添加暂存区2

---
+++ src/main/java/com/jiang/gitdemo/test.java
@@ -0,0 +1,14 @@
+package com.jiang.gitdemo;
+
+/**
+ * 测试
+ *
+ * @author JiangShuoKun
+ * @since 2023/6/7 17:59
+ **/
+public class test {
+    public static void main(String[] args) {
+        System.out.println("测试");
+        System.out.println("测试2");
+    }
+}
