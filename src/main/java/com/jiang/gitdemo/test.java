4899d0f82e85e2e35570b2f9f4178b749d364ca9
JiangShuoKun <923481817@qq.com>
2023-06-08 15:08:33 +0800

    idea的dev分支添加本地库5

--- src/main/java/com/jiang/gitdemo/test.java
+++ src/main/java/com/jiang/gitdemo/test.java
@@ -12,6 +12,6 @@ public class test {
         System.out.println("测试2");
         System.out.println("测试3");
         System.out.println("测试4");
-        System.out.println("测试5");
+        System.out.println("测试develop分支");
     }
 }
